a5296c36c78d3972d951bc334bd2c3fc36c34988
w-rhino <73589778+w-rhino@users.noreply.github.com>
2020-10-29 14:14:01 +0900

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -3,59 +3,12 @@ from discord.ext import commands
 import os
 import traceback
 
-#正規表現用ライブラリ
-import re
-import random
+# nDnダイスを呼び出す
+import nDnDICE
+
 
 bot = commands.Bot(command_prefix='$')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
-#ダイス用正規表現
-pattern = '\$\d{1,3}d\d{1,3}|\$\d{1,3}D\d{1,3}'
-split_pattern = 'd|D'
-
-#入力した文字がnDnに合致するか
-def judge_nDn(src):
-    repatter = re.compile(pattern)
-    result = repatter.fullmatch(src)
-    if result is not None:
-        return True
-    return False
-
-#nDnの数字を前半と後半に分ける
-def split_nDn(src):
-    src2 = re.sub('\$','',src)
-    return re.split(split_pattern,src2)
-
-#ダイスロール
-def roll_nDn(src):
-    result = []
-    sum_dice = 0
-    roll_index = split_nDn(src)
-    roll_count = int(roll_index[0])
-    nDice = int(roll_index[1])
-    
-    for i in range(roll_count):
-        tmp = random.randint(1,nDice)
-        result.append(tmp)
-        sum_dice = sum_dice + tmp
-    
-    is1dice = True if roll_count == 1 else False
-    
-    return result,sum_dice,is1dice
-
-#入力と出力
-def nDn(text):
-    if judge_nDn(text):
-        result,sum_dice,is1dice = roll_nDn(text)
-        spl = split_nDn(text)
-        if is1dice:
-            return spl[1] +'面ダイスを1回振ります。' + '\n出目：' + str(sum_dice)
-        else:
-            return spl[1] + '面ダイスを' + spl[0] + '回振ります。' + '\n出目：' + str(result) + '\n合計：' + str(sum_dice)
-    else:
-        return None
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -70,10 +23,11 @@ async def on_message(message):
     if message.author.bot:
         return
     # ダイスロール処理
-    msg = message.content
-    result = nDn(msg) 
-    if result is not None:
-        await message.channel.send(message.author.name + 'さんのダイスロール\n' + result)
+    if message.content == '\$\d{1,3}d\d{1,3}|\$\d{1,3}D\d{1,3}'
+        msg = message.content
+        num, times, result, sum = nDn(msg)
+        if result is not None:
+            await message.channel.send(message.author.name + 'さんのダイスロール\n' + num + '面ダイスを' + times + '回振ります。\n出目：' + str(result) + '\n合計：' + str(sum))
 
 @bot.command()
 async def ping(ctx):
