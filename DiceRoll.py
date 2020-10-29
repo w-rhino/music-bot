9c27b6c0ce537728e104e7d5aa9b8c2c8fe50312
w-rhino <73589778+w-rhino@users.noreply.github.com>
2020-10-29 14:58:15 +0900

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -3,8 +3,56 @@ from discord.ext import commands
 import os
 import traceback
 
-# nDnダイスを呼び出す
-import nDnDICE
+#正規表現用ライブラリ
+import re
+import random
+
+# nDnダイスモジュール
+
+#ダイス用正規表現
+pattern = '\$\d{1,3}d\d{1,3}|\$\d{1,3}D\d{1,3}'
+split_pattern = 'd|D'
+
+#入力した文字がnDnに合致するか
+def judge_nDn(src):
+    repatter = re.compile(pattern)
+    result = repatter.fullmatch(src)
+    if result is not None:
+        return True
+    return False
+
+#nDnの数字を前半と後半に分ける
+def split_nDn(src):
+    src2 = re.sub('\$','',src)
+    return re.split(split_pattern,src2)
+
+#ダイスロール
+def roll_nDn(src):
+    result = []
+    sum_dice = 0
+    roll_index = split_nDn(src)
+    roll_count = int(roll_index[0])
+    nDice = int(roll_index[1])
+    
+    for i in range(roll_count):
+        tmp = random.randint(1,nDice)
+        result.append(tmp)
+        sum_dice = sum_dice + tmp
+    
+    is1dice = True if roll_count == 1 else False
+    
+    return result,sum_dice,is1dice
+
+#入力と出力
+def nDn(text):
+    if judge_nDn(text):
+        result,sum_dice,is1dice = roll_nDn(text)
+        spl = split_nDn(text)
+        return spl[1],spl[0],result,sum_dice
+    else:
+        return None
+    
+####################
 
 bot = commands.Bot(command_prefix='$')
 token = os.environ['DISCORD_BOT_TOKEN']
