70ca849cc1444523d330afc9fdc828acfa561445
w-rhino <73589778+w-rhino@users.noreply.github.com>
2020-10-29 16:04:41 +0900

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -72,7 +72,7 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 @bot.command()
-async def r(ctx, arg)
+async def r(ctx, arg):
     num, times, result, sum_dice = nDn(arg)
     if result is not None:
             await message.channel.send(message.author.name + 'さんのダイスロール\n' + num + '面ダイスを' + times + '回振ります。\n出目：' + str(result) + '\n合計：' + str(sum_dice))
