debc41afb1575b7ac8257d6e27d13e5de46aa436
w-rhino <daichi.sone@gmail.com>
2020-11-13 10:52:32 +0900

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -280,9 +280,11 @@ async def shuffle(ctx):
 async def queue(ctx):
     global music_queue
     
+    sublist = music_queue[:10]
+    
     embed=discord.Embed(title= '現在の再生リスト(10曲分)',color=0xffa030)
     embed.add_field(name = "", value = "Now：" + current_music[1])
-    for count, value in enumerate(music_queue[:10], 1):
+    for count, value in enumerate(sublist, 1):
         embed.add_field(name="", value= str(count) + '：' + value[1], inline=False)
     await ctx.send(embed=embed)
     
